5f82cd645f7fb450c972c9fcf93b174f83ca9897
lukasbicus <ing.lukas.bicus@gmail.com>
2023-03-31 21:13:52 +0200

    Display some components on PokemonsPage

--- frontend/pages/pokemons/index.tsx
+++ frontend/pages/pokemons/index.tsx
@@ -1,7 +1,21 @@
+// @ts-ignore
+import { Button, Loading, ProgressBar, Search } from '@carbon/react'
 import React from 'react'
 
 export const PokemonsPage: React.FC = () => {
-  return <div>Dummy pokemons page</div>
+  return (
+    <div>
+      Hi there Dummy pokemons page
+      <Button>Hi there</Button>
+      <Loading description="Active loading indicator" withOverlay={false} />
+      <ProgressBar
+        label="Progress bar label"
+        helperText="Optional helper text"
+        value={75}
+      />
+      <Search />
+    </div>
+  )
 }
 
 export default PokemonsPage
