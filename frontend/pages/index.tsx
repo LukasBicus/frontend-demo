981f11814c2b10d7822fc7a9dc10f4b0ab892543
lukasbicus <ing.lukas.bicus@gmail.com>
2023-03-31 14:15:28 +0200

    Revert "Setup carbon."
    This reverts commit 3a04ccb84f703566640868af08c7295fc1af05f4.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,3 @@
-import { Button } from 'carbon-components-react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
@@ -17,7 +16,6 @@ export default function Home() {
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
-            <Button>HI carbon</Button>
           </p>
           <div>
             <a
