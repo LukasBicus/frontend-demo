05f82bb3cf5281e229ac261e90bfe40086cc094e
lukasbicus <ing.lukas.bicus@gmail.com>
2023-04-15 13:26:28 +0200

    Cleanup

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-// @ts-ignore
 import { Button } from '@carbon/react'
 
 export default function Home() {
