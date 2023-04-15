1da0e414f1e7adf938192667117f612cf7a28423
lukasbicus <ing.lukas.bicus@gmail.com>
2023-04-15 09:53:56 +0200

    Style button by overriding it's styles.

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,13 +1,7 @@
 import 'reset-css'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
-// @ts-ignore
-import { Theme } from '@carbon/react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Theme theme="g100">
-      <Component {...pageProps} />
-    </Theme>
-  )
+  return <Component {...pageProps} />
 }
