bf08621fcd622fce6cc57bd5755cd69a709538ff
lukasbicus <ing.lukas.bicus@gmail.com>
2023-04-15 13:43:51 +0200

    Setup css (#2)
    * Mark setup css as done :)
    
    * Add basic sass configuration
    
    * Install @carbon/react
    
    * Add css-reset
    
    * Remove globals.css
    
    * Add pokemons page
    
    * Use node v18. Reinstall packages.
    
    * Add globals
    
    * Move next, react one major down due @carbon/react
    
    * Setup carbon.
    
    * Revert "Setup carbon."
    
    This reverts commit 3a04ccb84f703566640868af08c7295fc1af05f4.
    
    * Add Carbon II
    
    * Display some components on PokemonsPage
    
    * Style button by overriding it's styles.
    
    * Setup prettier for sass
    
    * Modify theme.
    
    * Add tertiary button
    
    * Add more components
    
    * Add @carbon/icons-react
    
    * Add Icon button
    
    * Style ContentSwitcher
    
    * Update css
    
    * Cleanup
    
    * Reverting to latest versions (next, react, react-dom)

---
+++ frontend/feelings.md
@@ -0,0 +1,5 @@
+# What makes me sad
+1. no typescript support for @carbon/react 
+
+# What makes me happy
+1. a nice storybook with examples and documentation
---
+++ frontend/pages/pokemons/index.tsx
@@ -0,0 +1,85 @@
+import {
+  Button,
+  Loading,
+  ProgressBar,
+  Search,
+  ContentSwitcher,
+  Switch,
+  Dropdown,
+  IconButton,
+} from '@carbon/react'
+import { Add, Edit } from '@carbon/icons-react'
+import React from 'react'
+
+const items = [
+  {
+    id: 'option-0',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    id: 'option-1',
+    text: 'Option 1',
+  },
+  {
+    id: 'option-2',
+    text: 'Option 2',
+  },
+  {
+    id: 'option-3',
+    text: 'Option 3 - a disabled item',
+    disabled: true,
+  },
+  {
+    id: 'option-4',
+    text: 'Option 4',
+  },
+  {
+    id: 'option-5',
+    text: 'Option 5',
+  },
+]
+
+export const PokemonsPage: React.FC = () => {
+  return (
+    <div>
+      Hi there Dummy pokemons page
+      <Button isExpressive>Hi there</Button>
+      <Button kind="tertiary" isExpressive>
+        Hi there
+      </Button>
+      <IconButton label="123 label">
+        <Edit />
+      </IconButton>
+      <Button
+        renderIcon={Add}
+        iconDescription="Icon Description"
+        hasIconOnly
+        onClick={() => console.log('onClick')}
+      />
+      <ContentSwitcher onChange={() => {}}>
+        <Switch name="one" text="First section" />
+        <Switch name="two">Second section</Switch>
+        <Switch name="three" text="Third section" />
+      </ContentSwitcher>
+      <Loading description="Active loading indicator" withOverlay={false} />
+      <ProgressBar
+        label="Progress bar label"
+        helperText="Optional helper text"
+        value={75}
+      />
+      <Search />
+      <Dropdown
+        id="inline"
+        titleText="Inline dropdown label"
+        label="Dropdown menu options"
+        type="inline"
+        items={items}
+        itemToString={(item: { id: string; text: string }) =>
+          item ? item.text : ''
+        }
+      />
+    </div>
+  )
+}
+
+export default PokemonsPage
