d715efdd6935358be2d593525d61e3f6c2bf4753
lukasbicus <ing.lukas.bicus@gmail.com>
2023-04-17 15:12:59 +0200

    Add first query

--- frontend/components/pokemons/graphql.ts
+++ frontend/components/pokemons/graphql.ts
@@ -1,14 +1,22 @@
 import { gql } from '@/__generated__'
 
-const GET_POKEMONS = gql(`
+gql(`
+  fragment Edge on Pokemon {
+    id
+    name
+    classification
+    types
+    isFavorite
+    image
+  }
+
   query GetPokemons($limit: Int!, $offset: Int!) {
     pokemons(query:{limit: $limit, offset: $offset}) {
       limit
       offset
       count
       edges {
-        id
-        name
+        ...Edge
       }
     }
   }
