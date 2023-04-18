08df1e87ef6176aa448c044ace1dc82e36c330ac
lukasbicus <ing.lukas.bicus@gmail.com>
2023-04-18 16:22:13 +0200

    Generate (un)favoritePokemon

--- frontend/components/common/graphql.ts
+++ frontend/components/common/graphql.ts
@@ -5,3 +5,21 @@ export const GET_POKEMON_TYPES = gql`
     pokemonTypes
   }
 `
+
+export const FAVORITE_POKEMON = gql`
+  mutation favoritePokemon($id: ID!) {
+    favoritePokemon(id: $id) {
+      id
+      isFavorite
+    }
+  }
+`
+
+export const UN_FAVORITE_POKEMON = gql`
+  mutation unFavoritePokemon($id: ID!) {
+    unFavoritePokemon(id: $id) {
+      id
+      isFavorite
+    }
+  }
+`
