fb91e1236c0a52ee11988c837e77c1235314e73a
lukasbicus <ing.lukas.bicus@gmail.com>
2023-04-18 09:16:03 +0200

    Setup types & pageReducer.

--- frontend/components/pokemons/types.ts
+++ frontend/components/pokemons/types.ts
@@ -1,4 +1,4 @@
-export enum ContentSwitcherOptions {
+export enum ContentSwitcherMode {
   All = 'all',
   Favorites = 'favorites',
 }
@@ -7,3 +7,43 @@ export enum ViewMode {
   ListView = 'listView',
   GridView = 'gridView',
 }
+
+export interface IPageState {
+  contentSwitchMode: ContentSwitcherMode
+  viewMode: ViewMode
+  search?: string
+  type?: string
+}
+
+export enum PageActionTypes {
+  SET_CONTENT_SWITCH_MODE = 'SET_CONTENT_SWITCH_MODE',
+  SET_VIEW_MODE = 'SET_VIEW_MODE',
+  SET_SEARCH = 'SET_SEARCH',
+  SET_TYPE = 'SET_TYPE',
+}
+
+export interface ISetContentSwitchModeAction {
+  type: PageActionTypes.SET_CONTENT_SWITCH_MODE
+  payload: ContentSwitcherMode
+}
+
+export interface ISetViewModeAction {
+  type: PageActionTypes.SET_VIEW_MODE
+  payload: ViewMode
+}
+
+export interface ISetSearchAction {
+  type: PageActionTypes.SET_SEARCH
+  payload: string
+}
+
+export interface ISetTypeAction {
+  type: PageActionTypes.SET_TYPE
+  payload: string
+}
+
+export type PageAction =
+  | ISetContentSwitchModeAction
+  | ISetViewModeAction
+  | ISetSearchAction
+  | ISetTypeAction
