e0100c068df3c65bdca256cbad6216e0e8b83fe3
lukasbicus <ing.lukas.bicus@gmail.com>
2023-04-18 14:56:33 +0200

    Add some visualisation

--- frontend/components/common/Popularity.tsx
+++ frontend/components/common/Popularity.tsx
@@ -1,5 +1,7 @@
 'use client'
 import { Pokemon } from '@/__generated__/graphql'
+import { Favorite, FavoriteFilled } from '@carbon/icons-react'
+import { IconButton } from '@carbon/react'
 import React from 'react'
 
 interface IPopularityProps {
@@ -12,6 +14,12 @@ export const Popularity: React.FC<IPopularityProps> = ({
   return (
     <div>
       {id}: {String(isFavorite)}
+      <IconButton kind="ghost" size="lg" label="Add pokemon to favorites">
+        <Favorite />
+      </IconButton>
+      <IconButton kind="ghost" size="lg" label="Remove pokemon from favorites">
+        <FavoriteFilled />
+      </IconButton>
     </div>
   )
 }
