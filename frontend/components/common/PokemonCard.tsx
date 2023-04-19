3c8373459ce6d3f235f8233721ad27afe5b78945
lukasbicus <ing.lukas.bicus@gmail.com>
2023-04-19 11:12:19 +0200

    Style Card a bit.

--- frontend/components/common/PokemonCard.tsx
+++ frontend/components/common/PokemonCard.tsx
@@ -1,5 +1,7 @@
 import { Pokemon } from '@/__generated__/graphql'
 import { Popularity } from '@/components/common/Popularity'
+import styles from '@/styles/pokemonCard.module.scss'
+import Image from 'next/image'
 import React from 'react'
 
 interface IPokemonCardProps {
@@ -11,8 +13,14 @@ export const PokemonCard: React.FC<IPokemonCardProps> = ({
 }: IPokemonCardProps) => {
   return (
     <div>
-      Card: {pokemon.name}
-      <Popularity pokemon={pokemon} />
+      <Image src={pokemon.image} alt={pokemon.name} width={58} height={58} />
+      <div className={styles.footer}>
+        <div className={styles.description}>
+          <span className={styles.name}>{pokemon.name}</span>
+          <span>{pokemon.types.join(', ')}</span>
+        </div>
+        <Popularity pokemon={pokemon} />
+      </div>
     </div>
   )
 }
