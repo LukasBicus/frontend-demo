83975a6e944e1eccea6768264a9307c3ae87f55d
lukasbicus <ing.lukas.bicus@gmail.com>
2023-04-19 11:22:27 +0200

    Style Card a bit more.

--- frontend/components/common/PokemonCard.tsx
+++ frontend/components/common/PokemonCard.tsx
@@ -1,6 +1,7 @@
 import { Pokemon } from '@/__generated__/graphql'
 import { Popularity } from '@/components/common/Popularity'
 import styles from '@/styles/pokemonCard.module.scss'
+import { AspectRatio } from '@carbon/react'
 import Image from 'next/image'
 import React from 'react'
 
@@ -13,7 +14,11 @@ export const PokemonCard: React.FC<IPokemonCardProps> = ({
 }: IPokemonCardProps) => {
   return (
     <div>
-      <Image src={pokemon.image} alt={pokemon.name} width={58} height={58} />
+      <div className={styles.imageWrapper}>
+        <AspectRatio>
+          <Image src={pokemon.image} alt={pokemon.name} fill />
+        </AspectRatio>
+      </div>
       <div className={styles.footer}>
         <div className={styles.description}>
           <span className={styles.name}>{pokemon.name}</span>
