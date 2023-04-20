f12fbdb1aabdf4428d6062515b0745f47fbdf557
lukasbicus <ing.lukas.bicus@gmail.com>
2023-04-20 15:52:18 +0200

    Update favicon

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Pokemons</title>
         <meta name="description" content="All pokemons page" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/poke-favicon.svg" />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
       <LoadingProvider>
         <Component {...pageProps} />
