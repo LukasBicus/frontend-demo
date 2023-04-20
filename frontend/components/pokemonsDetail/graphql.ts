832eccbf0c12a632c8ac0b3d2aa43e5e5b122224
lukasbicus <ing.lukas.bicus@gmail.com>
2023-04-20 07:23:32 +0200

    Update graphql schema

--- frontend/components/pokemonsDetail/graphql.ts
+++ frontend/components/pokemonsDetail/graphql.ts
@@ -1,13 +1,35 @@
 import { gql } from '@apollo/client'
 
+const DIMENSION_FIELDS = gql`
+  fragment DimensionFields on PokemonDimension {
+    minimum
+    maximum
+  }
+`
+
 const POKEMON_DETAIL_FIELDS = gql`
+  ${DIMENSION_FIELDS}
   fragment PokemonDetailFields on Pokemon {
     id
     name
-    classification
     types
     isFavorite
     image
+    weight {
+      ...DimensionFields
+    }
+    height {
+      ...DimensionFields
+    }
+    maxCP
+    maxHP
+    sound
+    evolutions {
+      id
+      name
+      isFavorite
+      image
+    }
   }
 `
 
