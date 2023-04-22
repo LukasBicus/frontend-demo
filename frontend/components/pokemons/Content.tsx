844bd79923c865931cfdd458ae401112eee71ac3
lukasbicus <ing.lukas.bicus@gmail.com>
2023-04-22 14:52:37 +0200

    Update Content

--- frontend/components/pokemons/Content.tsx
+++ frontend/components/pokemons/Content.tsx
@@ -8,7 +8,7 @@ import { ScrollContainer } from '@/components/common/ScrollContainer'
 import { ListItem } from '@/components/pokemons/ListItem'
 import { getClient } from '@/lib/apolloClient'
 import styles from '@/styles/pokemons.module.scss'
-import React, { useEffect } from 'react'
+import React, { useCallback, useEffect } from 'react'
 import { ContentSwitcherMode, IPageState, ViewMode } from './types'
 
 interface IContentProps {
@@ -19,28 +19,30 @@ export const Content: React.FC<IContentProps> = ({
   pageState,
 }: IContentProps) => {
   const client = getClient()
-  const { data, previousData, loading, error } = useGetPokemonsQuery({
-    client,
-    fetchPolicy:
-      pageState.contentSwitchMode === ContentSwitcherMode.Favorites
-        ? 'network-only'
-        : 'cache-first',
-    variables: {
-      query: {
-        offset: 0,
-        limit: 12,
-        search: pageState.search ?? null,
-        filter: {
-          type: pageState.type ?? null,
-          isFavorite:
-            pageState.contentSwitchMode === ContentSwitcherMode.Favorites
-              ? true
-              : null,
+  const { data, previousData, loading, error, fetchMore } = useGetPokemonsQuery(
+    {
+      client,
+      fetchPolicy:
+        pageState.contentSwitchMode === ContentSwitcherMode.Favorites
+          ? 'network-only'
+          : 'cache-first',
+      variables: {
+        query: {
+          offset: 0,
+          limit: 12,
+          search: pageState.search ?? null,
+          filter: {
+            type: pageState.type ?? null,
+            isFavorite:
+              pageState.contentSwitchMode === ContentSwitcherMode.Favorites
+                ? true
+                : null,
+          },
         },
+        withoutTypes: false,
       },
-      withoutTypes: false,
     },
-  })
+  )
   const { showLoading, hideLoading } = useLoading()
   useEffect(() => {
     if (loading) {
@@ -49,32 +51,65 @@ export const Content: React.FC<IContentProps> = ({
       hideLoading()
     }
   }, [loading, showLoading, hideLoading])
+  useEffect(() => {
+    console.log('data changed', data)
+  }, [data])
+  const handleScrollNearEndOfTheContainer = useCallback(async () => {
+    console.log('Scrolled near the end', data)
+    if (data) {
+      console.log('Firing fetch')
+      try {
+        const variables = {
+          query: {
+            offset: data?.pokemons.offset + 12,
+            limit: 12,
+            search: pageState.search ?? null,
+            filter: {
+              type: pageState.type ?? null,
+              isFavorite:
+                pageState.contentSwitchMode === ContentSwitcherMode.Favorites
+                  ? true
+                  : null,
+            },
+          },
+          withoutTypes: false,
+        }
+        console.log('variables: ', variables)
+        const r = await fetchMore({
+          variables,
+        })
+        console.log('result', r)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  }, [data, fetchMore, pageState])
   if (error) {
     return <InlineError errorMessage="Something went wrong" />
   }
   const pokemons = data?.pokemons.edges ?? previousData?.pokemons.edges ?? []
   return (
     <>
-      {pageState.viewMode === ViewMode.ListView ? (
-        <div className={styles.list}>
-          {pokemons.map((pokemon) => (
-            <ListItem pokemon={pokemon} key={pokemon.id} />
-          ))}
-        </div>
-      ) : (
-        <ScrollContainer
-          className={styles.grid}
-          onScrollNearEndOfTheContainer={() => {
-            console.log('Scrolled near the end')
-          }}
-        >
-          {pokemons.map((pokemon) => (
-            <div className={styles.gridGap} key={pokemon.id}>
-              <PokemonCard pokemon={pokemon} />
-            </div>
-          ))}
-        </ScrollContainer>
-      )}
+      {pokemons.length ? (
+        pageState.viewMode === ViewMode.ListView ? (
+          <div className={styles.list}>
+            {pokemons.map((pokemon) => (
+              <ListItem pokemon={pokemon} key={pokemon.id} />
+            ))}
+          </div>
+        ) : (
+          <ScrollContainer
+            className={styles.grid}
+            onScrollNearEndOfTheContainer={handleScrollNearEndOfTheContainer}
+          >
+            {pokemons.map((pokemon) => (
+              <div className={styles.gridGap} key={pokemon.id}>
+                <PokemonCard pokemon={pokemon} />
+              </div>
+            ))}
+          </ScrollContainer>
+        )
+      ) : null}
 
       {!loading && pokemons.length === 0 && (
         <div className={styles.noResults}>No results</div>
