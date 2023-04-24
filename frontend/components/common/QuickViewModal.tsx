e470b7aeca5506d965245aa784ef157972cc2920
lukasbicus <ing.lukas.bicus@gmail.com>
2023-04-24 07:26:52 +0200

    Implement useQuickViewModal hook

--- frontend/components/common/QuickViewModal.tsx
+++ frontend/components/common/QuickViewModal.tsx
@@ -10,8 +10,52 @@
  * style modal
  */
 
+import {
+  PokemonModalFieldsFragment,
+  useGetPokemonModalLazyQuery,
+} from '@/__generated__/graphql'
+import { useLoading } from '@/components/common/LoadingProvider'
+import { ToastKind, useToastContext } from '@/components/common/ToastProvider'
+import { getClient } from '@/lib/apolloClient'
 import { ComposedModal, ModalBody, ModalHeader } from '@carbon/react'
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+
+export const useQuickViewModal = () => {
+  const { showLoading, hideLoading } = useLoading()
+  const { showToast } = useToastContext()
+  const client = getClient()
+  const [loadModal] = useGetPokemonModalLazyQuery({ client })
+  const [modalData, setModalData] = useState<null | PokemonModalFieldsFragment>(
+    null,
+  )
+  const getOpenModalHandler = useCallback(
+    (id: string) => async () => {
+      try {
+        showLoading()
+        const { data } = await loadModal({ variables: { id } })
+        if (data) {
+          setModalData(data.pokemonById)
+        }
+      } catch (e) {
+        showToast({
+          kind: ToastKind.Error,
+          title: 'Failed to load modal data',
+        })
+      } finally {
+        hideLoading()
+      }
+    },
+    [hideLoading, loadModal, showLoading, showToast],
+  )
+  const closeModal = useCallback(() => {
+    setModalData(null)
+  }, [])
+  return {
+    getOpenModalHandler,
+    modalData,
+    closeModal,
+  }
+}
 
 interface IQuickViewModalProps {
   open: boolean
