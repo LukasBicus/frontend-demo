a40c6dcc7ecd094151e742b005e3f678272ca70f
lukasbicus <ing.lukas.bicus@gmail.com>
2023-04-24 06:22:55 +0200

    Draw plan

---
+++ frontend/components/common/QuickViewModal.tsx
@@ -0,0 +1,11 @@
+/**
+ * implement hook, types, getMore info about pokemon (see playground)
+ * add show modal button to PokemonCard
+ * develop dummy modal component
+ * create hook
+ * - getOpenModalHandler(id: pokemonId) - starts loading, starts fetching data for pokemon with id id, opens modal, stops loading
+ * - close modal
+ * - modalData: PokemonModalFields | null
+ * show data in modal
+ * style modal
+ */
